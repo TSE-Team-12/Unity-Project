64360607a7d922952fd146d7b418de8995cc9ac7
DJSIDESWIPE <djleverton@outlook.com>
2020-01-30 12:25:54 +0000

    Movement
    Added player movement

---
+++ Group12/Assets/Player/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
