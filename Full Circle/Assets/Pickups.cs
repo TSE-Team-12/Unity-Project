0635a2aea48d14a0eda3ea024aa4a28804ca64a4
JackBassindale <60317021+JackBassindale@users.noreply.github.com>
2020-02-15 15:28:45 +0000

    Pickups
    You pick them up

---
+++ Full Circle/Assets/Pickups.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+
+    public Text healthText;            
+    public Text ammoText;
+    public Text batteryText;
+
+    private int health;
+    private int ammo;
+    private int battery;
+
+     void Start()
+    {
+
+        health = 100;
+        ammo = 0;
+        battery = 100;
+
+    }
+
+   
+    void OnTriggerEnter(Collider other)
+    {
+
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("HealthPickup"))
+        {
+            other.gameObject.SetActive(false);
+            health = health + 20;
+            SetHealthText ();
+        }
+
+        if (other.gameObject.CompareTag("BatteryPickup"))
+        {
+            other.gameObject.SetActive(false);
+            battery = battery + 20;
+            SetBatteryText();
+        }
+
+        if (other.gameObject.CompareTag("AmmoPickup"))
+        {
+            other.gameObject.SetActive(false);
+            ammo = ammo + 5;
+            SetAmmoText();
+        }
+
+        void SetHealthText()
+        {
+            healthText.text = "Health: " + health.ToString();
+        }
+        void SetAmmoText()
+        {
+            ammoText.text = "Ammo: " + ammo.ToString();
+        }
+        void SetBatteryText()
+        {
+            batteryText.text = "Battery: " + battery.ToString();
+        }
+    }
+}
