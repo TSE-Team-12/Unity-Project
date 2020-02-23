9db765f1ac17f5b3a10e7c0796f8fa69fc6f479b
DJSIDESWIPE <djleverton@outlook.com>
2020-02-23 19:25:24 +0000

    added new bulletbehaviour script to master version

---
+++ Full Circle/Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform player; // Reference to Player
+    void LateUpdate() // We use LateUpdate to ensure player has moved before updating Minimap (LateUpdate is called after Update and FixedUpdate).
+    {
+        Vector3 newPosition = player.position; // New position of our Minimap set to position of player.
+        newPosition.y = transform.position.y; 
+        transform.position = newPosition;
+    }
+
+
+}
