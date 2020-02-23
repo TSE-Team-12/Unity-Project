2942d570ecda6fe94e3cc82cd020a6c33b621363
Connagh-Lincoln <18672733@students.lincoln.ac.uk>
2020-02-23 14:08:05 +0000

    Merge from DJxC Branch - Needs updated bullet script

---
+++ Full Circle/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float MaxHealth = 100;
+    public float CurHealth;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            CurHealth -= 25;
+            Destroy(other.gameObject, 0f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurHealth <= 0)
+        {
+            Destroy(this.gameObject, 0f);
+        }
+    }
+}
