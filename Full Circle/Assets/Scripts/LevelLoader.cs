14cedc96e2af16a0dbee5f9f1f7d344448f30f3b
JackPadmore <18675942@students.lincoln.ac.uk>
2020-02-26 20:18:11 +0000

    Framework for Scene Transitions
    +Added a blank scene to be added upon (named "second level")
    +Added prefab upon which scene transitions can be added and removed.
    +Added transition between first and second level.
    +Added simple (placeholder) red fade animation during level one and two transition.
    +Made scene transition trigger upon player reaching the "win floor".
    +Created animations folders which currently containts animations for scene transitions, however this can also contain any additional animations within the project.
    -moved MAINMENU script into scripts folder.

---
+++ Full Circle/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f; //Transition pause time
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        LoadNextLevel();
+    }
+
+
+    public void LoadNextLevel()//custom function to transition to the next level.
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); //takes the current level and transition to the next index via the build order index.
+    }
+
+    IEnumerator LoadLevel(int levelIndex) //co-routine
+    {
+        //Play Transition Animation
+        transition.SetTrigger("Start");
+        //Wait for Animation to Finish
+        yield return new WaitForSeconds(transitionTime);
+        //Load the new Scene
+        SceneManager.LoadScene(levelIndex); 
+    }
+}
