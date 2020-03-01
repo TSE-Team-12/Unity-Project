e21e04209fc72593280c849a18f9f9fb5abfb58b
DJSIDESWIPE <djleverton@outlook.com>
2020-03-01 10:43:54 +0000

    Cleaned up the hierarcy

--- Full Circle/Assets/Scripts/CameraMovement.cs
+++ Full Circle/Assets/Scripts/CameraMovement.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public Transform PlayerTransform; // Gets Player Transform
+    private Vector3 CameraOffSet; // How far the camera is away from the player
 
-    public Transform PlayerTransform;
-    private Vector3 CameraOffSet;
-
-    public float SmoothFactor = 0.125f;
+    public float SmoothFactor = 0.125f; // Smoothness factor, oolala
 
     // Start is called before the first frame update
     void Start()
     {
-        CameraOffSet = transform.position - PlayerTransform.position;
+        CameraOffSet = transform.position - PlayerTransform.position; // Gets position of camera and checks it against the player's position
         
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 newPos = PlayerTransform.position + CameraOffSet;
-        Vector3 SmoothPos = Vector3.Lerp(transform.position, newPos, SmoothFactor);
-        transform.position = SmoothPos;
+        Vector3 newPos = PlayerTransform.position + CameraOffSet; // If player moves, updates new position variable
+        Vector3 SmoothPos = Vector3.Lerp(transform.position, newPos, SmoothFactor); // Smooths camera transition
+        transform.position = SmoothPos; // Moves Camera
     }
 }
