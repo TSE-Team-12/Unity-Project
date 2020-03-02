3a374e22dd91e6d1d06bab4b866937b7ddc02775
DJSIDESWIPE <djleverton@outlook.com>
2020-03-02 13:32:50 +0000

    tidied up hierarchy and added updated hud

--- Full Circle/Assets/Scripts/Shooting.cs
+++ Full Circle/Assets/Scripts/Shooting.cs
@@ -6,31 +6,43 @@ public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
     public float timer = 25;
+    public int ammo = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public void AddAmmo(int ToAdd)
+    {
+        ammo += ToAdd;
     }
 
     void Shoot()
     {
         Instantiate(bullet, transform.position, transform.rotation);
+        ammo--;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (timer >= 25)
         {
-            if (Input.GetMouseButton(0))
+            if (ammo >= 1)
             {
-                Shoot();
-                timer = 0;
+                if (Input.GetMouseButton(0))
+                {
+                    Shoot();
+                    timer = 0;
+                }
+
             }
-        } else
+        }
+        else
         {
             timer++;
         }
-        
     }
 }
