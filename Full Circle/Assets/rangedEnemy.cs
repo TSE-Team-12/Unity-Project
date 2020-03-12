ec71c4479de6b6b6bd252982329bf43c30425db3
JackPadmore <43850659+JackPadmore@users.noreply.github.com>
2020-03-12 14:39:37 +0000

    Enemy Shoots Bullets
    +when the player is in range of the enemy attack range, the enemy will.
    +added an attack timer to the enemy to delay attacks.
    +added random variation to created bullets to make bullet less predictable to the player.

--- Full Circle/Assets/rangedEnemy.cs
+++ Full Circle/Assets/rangedEnemy.cs
@@ -6,10 +6,10 @@ public class rangedEnemy : MonoBehaviour
 {
     
     Transform player;
-    public float attackTimer = 10;
+    public float attackTimer = 60;
     public float attackDist = 10;
-    public GameObject bullet; 
-
+    public GameObject bullet;
+    float strayAmount = 10f;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -24,10 +24,23 @@ public class rangedEnemy : MonoBehaviour
             // Makes the ranged enemy face the player before attacking //
             Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z); //restricts ranged enemy rotation to the y axis.
             transform.LookAt(targetPosition);
-        } 
+            // Enemy Shoots when player is in range //
+            if (attackTimer >= 60)
+            {
+                enemyShoot();
+                attackTimer = 0;
+            }
+        }
+        if (attackTimer < 60){
+            attackTimer++;
+        }
     }
 
     void enemyShoot() {
-        Instantiate(bullet, transform.position, transform.rotation); 
+        var rndY = Random.Range(-strayAmount, strayAmount);
+        var enemyBullet = Instantiate(bullet, transform.position, transform.rotation); 
+        //Adds random bullet deviation to make the projectile a little less predictable //
+        enemyBullet.transform.Rotate(player.transform.position.x, rndY, player.transform.position.z);
+        
     }
 }
