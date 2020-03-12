879efac039e07ed385d0e41f0a87c57173a3b043
James Heselden <34611419+Iranaphor@users.noreply.github.com>
2020-03-12 15:28:47 +0000

    .Gitignore Fix 2/2

---
+++ Full Circle/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float MaxHealth = 100; // Enemy Max Health
+    public float CurHealth; // Enemy Current Health
+
+    public void TakeDamage(float Amount) // Take damage function (called by bullet)
+    {
+        CurHealth -= Amount; // Take 25 damage from current health
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurHealth = MaxHealth; // Set current health to Max Health, on start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurHealth <= 0) // If current health goes below 0
+        {
+            Destroy(this.gameObject, 0f); // Destroy enemy
+        }
+    }
+}
---
+++ Full Circle/Assets/Tutorial/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public static int CurHealth = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
