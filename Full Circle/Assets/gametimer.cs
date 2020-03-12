9896adaa1301e391c6636c5b5eafc63cc0a77f57
James Heselden <34611419+Iranaphor@users.noreply.github.com>
2020-03-12 15:36:48 +0000

    Yuemin - Game Timer

---
+++ Full Circle/Assets/gametimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gametimer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (finnished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f1");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+    void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+}
