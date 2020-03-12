79ab26168cbcbd3b9157f276f3165ec4a8356e71
JackPadmore <43850659+JackPadmore@users.noreply.github.com>
2020-03-12 14:09:58 +0000

    Initial Branch Commit
    +added script to Enemy(0) making them a ranged enemy (temporary for testing)
    +Enemies will face the player when the player reaches a certain distance to said enemy.

---
+++ Full Circle/Assets/rangedEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rangedEnemy : MonoBehaviour
+{
+    
+    Transform player;
+    public float attackTimer = 10;
+    public float attackDist = 10;
+    public GameObject bullet; 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player distance check before attacking //
+        if (Vector3.Distance(transform.position, player.position) < attackDist)
+        {
+            // Makes the ranged enemy face the player before attacking //
+            Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z); //restricts ranged enemy rotation to the y axis.
+            transform.LookAt(targetPosition);
+        } 
+    }
+
+    void enemyShoot() {
+        Instantiate(bullet, transform.position, transform.rotation); 
+    }
+}
