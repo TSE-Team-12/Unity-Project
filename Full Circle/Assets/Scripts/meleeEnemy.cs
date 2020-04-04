3a678d0ff6425e893bc357a790c59afcde844740
JackPadmore <18675942@students.lincoln.ac.uk>
2020-04-04 06:26:35 +0100

    Melee Enemy Foundation
    +created melee enemy
    +made melee enemy face and follow player within a certain range.
    +added foundation toward melee attack

---
+++ Full Circle/Assets/Scripts/meleeEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meleeEnemy : MonoBehaviour
+{
+    
+    Transform player;
+    public float attackTimer = 60;
+    public float detectDist = 10;
+    float moveSpeed = 3;
+
+   
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player distance check before moving//
+        if (Vector3.Distance(transform.position, player.position) < detectDist)
+        {
+            // Makes the ranged enemy face the player before attacking //
+            Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z); //restricts ranged enemy rotation to the y axis.
+            transform.LookAt(targetPosition);//enemy face player
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;//enemy moves toward player
+        }
+        if (attackTimer < 60)
+        {
+            attackTimer++;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) { 
+        if (collision.gameObject.tag == "Player")
+        {
+            if (attackTimer >= 60) {
+                Debug.Log("collision");
+                attackTimer = 0;
+            }
+        }
+    }
+}
